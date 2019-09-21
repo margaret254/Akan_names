7b36891b2af1f028e9c053efed05473fcf67c14a
margaret254 <maggiemwas91@gmail.com>
2019-09-21 21:13:32 +0300

    Declare variables century,year,month,date

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var century,year,month,date,d,dayValue;
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function validate(){
+    var
+}
