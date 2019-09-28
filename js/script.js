592c8cdb9b679384240e663442386bf56340d1eb
margaret254 <maggiemwas91@gmail.com>
2019-09-28 10:51:46 +0300

    Create a function akanNames in script.js file

--- js/script.js
+++ js/script.js
@@ -1,110 +1,45 @@
-var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+function formRefresh(){
+  document.getElementById("form_data").reset();
+}
 
-function validate() {
-  var genders = document.getElementsByName("gender");
-  if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2019 || document.myForm.year.value <=1963) {
-     alert( "Please provide a valid year of birth! " );
-     document.myForm.year.focus() ;
-     return false;
+// Event function on button
+function akanNames(){
+
+  var birthDate=document.getElementById("birthdate").value;
+  var dateEntered= birthDate.split("-");
+  var day=parseInt(dateEntered[2]);
+  var month=parseInt(dateEntered[1]);
+  var year=parseInt(dateEntered[0]);
+
+  //Centuary calculation
+  var centry=(year-1)/100+1; 
+  var dayOfTheWeek = ( centry/4 -2*centry-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
+      
+  document.getElementById("display").innerHTML=Math.round(dayOfTheWeek);
+  var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+  var maleNames=["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var gender=document.getElementById("gender").value;
+  var daysOfWeek=["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var dOfWeek = Math.round(dayOfTheWeek)
+
+  //form validation and output
+  if(!isNaN(dOfWeek)){
+      if(gender=="Male"){
+          document.getElementById("display").innerHTML= "Your akan names is"+ maleNames[Math.round(dayOfTheWeek)]+" and you were born on "+ daysOfWeek[dOfWeek];
+      }
+      else if(gender==="Female"){
+          document.getElementById("display").innerHTML="You were born on "+daysOfWeek[dOfWeek]+ " and Your akan name is " + femaleNames[Math.round(dayOfTheWeek)];
+      }else{
+          alert("Enter valid dates and choose your gender");
+          document.getElementById("display").innerHTML="";
+      }
   }
-  else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
-     alert( "Please provide your month of birth!" );
-     document.myForm.month.focus() ;
-     return false;
-  }
-  else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
-     alert( "Please provide a valid date that you were born in!" );
-     document.myForm.day.focus() ;
-     return false;
-  }
-  else if(genders[0].checked==false && genders[1].checked==false ) {
-      alert("You must select male or female");
-      return false;
-  }   
-  else{
-    return true ;
-  }
+          else{
+              alert("Enter valid birthday and choose your gender");
+              document.getElementById("display").innerHTML="";
+          }
   
-}
-
-function calculateDayValue(){
-  year = document.getElementById("year").value;
-  CC = parseInt(year.substring(0,2));
-  YY = parseInt(year.substring(2,4));
-  MM = parseInt(document.getElementById("month").value);
-  DD = parseInt(document.getElementById("date").value);
-  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-  console.log(d);
-  return (Math.floor(d));
-}
-
-function getGender(){
-  var genders = document.getElementsByName("gender");
-  if(genders[0].checked == true){
-    var gender = "male";
-  }
-  else if(genders[1].checked == true){
-    var gender = "female";
-  }
-  else{
-    return false;
-  }
-  switch(gender){
-    case "male":
-      if (dayValue == 1){
-        alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
-      }
-      else if(dayValue == 2){
-        alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
-      }
-      else if(dayValue == 3){
-        alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
-      }
-      else if(dayValue == 4){
-        alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
-      }
-      else if(dayValue == 5){
-        alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
-      }
-      else if(dayValue == 6){
-        alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
-      }
-      else if(dayValue == -0){
-        alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
-      }
-    break;
-    case "female":
-      if (dayValue == 1){
-        alert("You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0]+"!");
-      }
-      else if(dayValue == 2){
-        alert("You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1]+"!");
-      }
-      else if(dayValue == 3){
-        alert("You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2]+"!");
-      }
-      else if(dayValue == 4){
-        alert("You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3]+"!");
-      }
-      else if(dayValue == 5){
-        alert("You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4]+"!");
-      }
-      else if(dayValue == 6){
-        alert("You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5]+"!");
-      }else if(dayValue == -0){
-        alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
-      }
-    break
-    default:
-          
-  }
-}
-function findName(){
-  dayValue = calculateDayValue();
-  getGender();
+  
+  //call to the reFresh function
+  formRefresh();
 }
